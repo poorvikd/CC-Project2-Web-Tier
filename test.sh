45f1307db5cb187337ffbd8a5bb515590cb8a359
poorvikd <64829799+poorvikd@users.noreply.github.com>
2024-10-04 16:41:29 -0700

    final refactoring

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 if [ "$1" = "localhost" ]; then
-  python /Users/poorvikd/Documents/CloudComp/CSE546-Cloud-Computing/workload_generator/workload_generator.py --num_request $2 --url 'http://127.0.0.1:8000/' --image_folder "/Users/poorvikd/Documents/CloudComp/CSE546-Cloud-Computing/dataset/face_images_1000" --prediction_file "/Users/poorvikd/Documents/CloudComp/CSE546-Cloud-Computing/dataset/Classification Results on Face Dataset (1000 images).csv"
+  python /Users/poorvikd/Documents/CloudComp/CSE546-Cloud-Computing/workload_generator/workload_generator.py --num_request $2 --url 'http://127.0.0.1:8000/' --image_folder "/Users/poorvikd/Documents/CloudComp/CSE546-Cloud-Computing/dataset/face_images_100" --prediction_file "/Users/poorvikd/Documents/CloudComp/CSE546-Cloud-Computing/dataset/Classification Results on Face Dataset (100 images).csv"
 fi
 
 if [ "$1" = "ec2" ]; then
-  python /Users/poorvikd/Documents/CloudComp/CSE546-Cloud-Computing/workload_generator/workload_generator.py --num_request $2 --url 'http://100.24.155.186/' --image_folder "/Users/poorvikd/Documents/CloudComp/CSE546-Cloud-Computing/dataset/face_images_1000" --prediction_file "/Users/poorvikd/Documents/CloudComp/CSE546-Cloud-Computing/dataset/Classification Results on Face Dataset (1000 images).csv"
+  python /Users/poorvikd/Documents/CloudComp/CSE546-Cloud-Computing/workload_generator/workload_generator.py --num_request $2 --url 'http://3.226.168.170:5000/' --image_folder "/Users/poorvikd/Documents/CloudComp/CSE546-Cloud-Computing/dataset/face_images_1000" --prediction_file "/Users/poorvikd/Documents/CloudComp/CSE546-Cloud-Computing/dataset/Classification Results on Face Dataset (1000 images).csv"
 fi
