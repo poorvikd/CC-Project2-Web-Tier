0c677a5eb4aa98596a9154b49647168690daea22
poorvikd <64829799+poorvikd@users.noreply.github.com>
2024-10-04 12:14:28 -0700

    code cleanup

--- models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from database import Base
-
-class Face(Base):
-    __tablename__ = "faces"
-
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    name = Column(String)
-    image_name = Column(String)
